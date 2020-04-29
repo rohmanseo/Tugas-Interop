058e328295136543fe9a090f5ec7cb75e4641975
Yasta <rattyasta1@gmail.com>
2020-04-29 16:21:59 +0700

    register done

--- frontend/src/modules/register-component.js
+++ frontend/src/modules/register-component.js
@@ -1,13 +1,54 @@
 import React, { Component, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+const axios = require("axios");
+
+const api = axios.create({
+  baseURl: `http://127.0.0.1:8000/api/auth/register`,
+  responseType: `json`,
+});
 
 class RegisterComponent extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+
+    this.handleNameChange = (event) => {
+      this.setState({ name: event.target.value });
+    };
+
+    this.handleEmailChange = (event) => {
+      this.setState({ email: event.target.value });
+    };
+
+    this.handlePasswordChange = (event) => {
+      this.setState(
+        { password: event.target.value },
+        console.log(this.state.password)
+      );
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    let url = `http://127.0.0.1:8000/api/auth/register`;
+    axios
+      .post(url, {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+      })
+      .then();
+  };
   render() {
     return (
-        <body class="bg-gradient-primary">
-
+      <body class="bg-gradient-primary">
         <div class="container">
-      
           <div class="card o-hidden border-0 shadow-lg my-5">
             <div class="card-body p-0">
               <div class="row">
@@ -17,44 +58,82 @@ class RegisterComponent extends Component {
                     <div class="text-center">
                       <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                     </div>
-                    <form class="user">
+                    <form class="user" onSubmit={this.handleSubmit}>
                       <div class="form-group row">
-                        <div class="col-sm-6 mb-3 mb-sm-0">
-                          <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name"/>
-                        </div>
-                        <div class="col-sm-6">
-                          <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name"/>
+                        <div class="col-sm-12 mb-3 mb-sm-0">
+                          <input
+                            type="text"
+                            class="form-control form-control-user"
+                            id="exampleFirstName"
+                            placeholder="First Name"
+                            name="name"
+                            value={this.state.name}
+                            onChange={this.handleNameChange}
+                          />
                         </div>
                       </div>
                       <div class="form-group">
-                        <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address"/>
+                        <input
+                          type="email"
+                          class="form-control form-control-user"
+                          id="exampleInputEmail"
+                          placeholder="Email Address"
+                          value={this.state.email}
+                          onChange={this.handleEmailChange}
+                        />
                       </div>
                       <div class="form-group row">
                         <div class="col-sm-6 mb-3 mb-sm-0">
-                          <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password"/>
+                          <input
+                            type="password"
+                            class="form-control form-control-user"
+                            id="exampleInputPassword"
+                            placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.handlePasswordChange}
+                          />
                         </div>
                         <div class="col-sm-6">
-                          <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password"/>
+                          <input
+                            type="password"
+                            class="form-control form-control-user"
+                            id="exampleRepeatPassword"
+                            placeholder="Repeat Password"
+                          />
                         </div>
                       </div>
-                      <a href="login.html" class="btn btn-primary btn-user btn-block">
+                      <button
+                        type="submit"
+                        class="btn btn-primary btn-user btn-block"
+                      >
                         Register Account
-                      </a>
-                      <hr/>
-                      <a href="index.html" class="btn btn-google btn-user btn-block">
+                      </button>
+                      <hr />
+                      <a
+                        href="index.html"
+                        class="btn btn-google btn-user btn-block"
+                      >
                         <i class="fab fa-google fa-fw"></i> Register with Google
                       </a>
-                      <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                        <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                      <a
+                        href="index.html"
+                        class="btn btn-facebook btn-user btn-block"
+                      >
+                        <i class="fab fa-facebook-f fa-fw"></i> Register with
+                        Facebook
                       </a>
                     </form>
-                    <hr/>
+                    <hr />
                     <div class="text-center">
-                      <a class="small" href="forgot-password.html">Forgot Password?</a>
+                      <a class="small" href="forgot-password.html">
+                        Forgot Password?
+                      </a>
                     </div>
                     <div class="text-center">
-                      <Link to='/'>
-                      <a class="small" href="login.html">Already have an account? Login!</a>
+                      <Link to="/">
+                        <a class="small" href="login.html">
+                          Already have an account? Login!
+                        </a>
                       </Link>
                     </div>
                   </div>
@@ -62,18 +141,15 @@ class RegisterComponent extends Component {
               </div>
             </div>
           </div>
-      
         </div>
-      
+
         <script src="vendor/jquery/jquery.min.js"></script>
         <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-      
+
         <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-      
+
         <script src="js/sb-admin-2.min.js"></script>
-      
       </body>
-      
     );
   }
 }
